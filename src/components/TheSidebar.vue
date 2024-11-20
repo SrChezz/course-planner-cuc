6e8b9899cb534d6ef1903cdbc1ebbffe4f2818b9
Chezzby <itzchezvil17@gmail.com>
2024-11-19 19:46:03 -0500

    fix: sidebar links

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -10,13 +10,27 @@
         >Inicio</RouterLink
       >
       <a href="#" class="block py-2 px-4 font-medium">Cursos</a>
+
       <!-- Add more menu items as needed -->
+      <ul>
+        <li v-for="course in courses">
+          <RouterLink
+            :to="`/course/${course.id}`"
+            class="block py-2 px-4 text-gray-600 hover:bg-orange-100 hover:text-orange-500"
+            >{{ course.name }}</RouterLink
+          >
+        </li>
+      </ul>
     </nav>
   </aside>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
+import { useCoursesStore } from '@/stores/courses';
+
+const coursesStore = useCoursesStore();
+const courses = ref([]);
 
 const items = ref([
   {
@@ -25,31 +39,39 @@ const items = ref([
   {
     items: [
       {
-        label: "Inicio",
-        icon: "pi pi-home",
+        label: 'Inicio',
+        icon: 'pi pi-home',
         // shortcut: 'ctrl+h',
       },
     ],
   },
 
   {
-    label: "Cursos",
+    label: 'Cursos',
     items: [
       {
-        label: "Curso 01",
-        icon: "pi pi-book",
+        label: 'Curso 01',
+        icon: 'pi pi-book',
       },
       {
-        label: "Curso 02",
-        icon: "pi pi-book",
+        label: 'Curso 02',
+        icon: 'pi pi-book',
       },
       {
-        label: "Curso 03",
-        icon: "pi pi-book",
+        label: 'Curso 03',
+        icon: 'pi pi-book',
       },
     ],
   },
 ]);
+
+// Fetch courses from Pinia store
+const fetchCourses = async () => {
+  await coursesStore.fetchCourses();
+  courses.value = coursesStore.courses;
+};
+
+fetchCourses();
 </script>
 
 <style scoped>
